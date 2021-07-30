a052bb7da5e893274521f34da5811477eedfe5f8
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2021-07-30 18:17:28 +0600

    Added gem devise and auth

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # Shrine is a toolkit for handling file attachments
 gem "shrine", "~> 3.0"
+# Flexible authentication
+gem 'devise', '~> 4.2'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_090043) do
+ActiveRecord::Schema.define(version: 2021_07_29_131129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2021_07_29_090043) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "bio"
   end
 
   add_foreign_key "posts", "users"
