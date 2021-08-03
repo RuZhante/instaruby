96a3e327402e0c4fd29fffdc6524ac221c114ee7
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2021-08-03 20:50:49 +0600

    Added Follows

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_131129) do
+ActiveRecord::Schema.define(version: 2021_08_03_132406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -34,5 +41,7 @@ ActiveRecord::Schema.define(version: 2021_07_29_131129) do
     t.string "bio"
   end
 
+  add_foreign_key "follows", "users", column: "follower_id"
+  add_foreign_key "follows", "users", column: "following_id"
   add_foreign_key "posts", "users"
 end
