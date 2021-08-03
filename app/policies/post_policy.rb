6f18ed6853fd9b88334f8df179b2a0424ff27df9
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2021-08-03 21:34:11 +0600

    Added Policy

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,21 @@
+class PostPolicy
+  attr_reader :user, :post
+
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
+
+  def update?
+    user.posts.where(id: post.id).exists?
+  end
+
+  def edit?
+    user.posts.where(id: post.id).exists?
+  end
+
+  def destroy?
+    user.posts.where(id: post.id).exists?
+  end
+
+end
